919c724f436569c5c383a46707b306455aa89a65
John <flextions@gmail.com>
2019-01-27 15:47:02 -0500

    Set timer and fix end of quiz message

--- app.js
+++ app.js
@@ -5,83 +5,76 @@ data = [ // Elements include a question and answer
   // a = answer
   // r = reference
   {s:"Weights <b>Pre Mod</b> SB 505-00-0008", q:"Max Ramp (MRW)", a:18078, r:""},
-  {s:"Weights <b>Pre Mod</b> SB 505-00-0008", q:"Max Takeoff (MTOW)", a:17968, r:""},
-  {s:"Weights <b>Pre Mod</b> SB 505-00-0008", q:"Max Landing (MLW)", a:16865, r:""},
-  {s:"Weights <b>Pre Mod</b> SB 505-00-0008", q:"Max Zero Fuel (MZFW)", a:13999, r:""},
-  {s:"Weights <b>Post Mod</b> SB 505-00-0008", q:"Max Ramp (MRW)", a:18497, r:""},
-  {s:"Weights <b>Post Mod</b> SB 505-00-0008", q:"Max Takeoff (MTOW)", a:18387, r:""},
-  {s:"Weights <b>Post Mod</b> SB 505-00-0008", q:"Max Landing (MLW)", a:17042, r:""},
-  {s:"Weights <b>Post Mod</b> SB 505-00-0008", q:"Max Zero Fuel (MZFW)", a:14220, r:""},
-  {s:"Airspeeds", q:"VLO KIAS", a:250, r:""},
-  {s:"Airspeeds", q:"VLE KIAS", a:250, r:""},
-  {s:"Airspeeds", q:"Takeoff VMC KIAS", a:97, r:""},
-  {s:"Airspeeds", q:"Landing VMC (No Icing Conditions) KIAS", a:91, r:""},
-  {s:"Airspeeds", q:"Landing VMC (Icing Conditions) KIAS", a:95, r:""},
-  {s:"Airspeeds", q:"MMO", a:0.78, r:""},
-  {s:"Airspeeds", q:"VO KIAS", a:205, r:""},
-  {s:"Airspeeds", q:"VFE Flaps 1 KIAS", a:180, r:""},
-  {s:"Airspeeds", q:"VFE Flaps 2&3 KIAS", a:170, r:""},
-  {s:"Airspeeds", q:"VFE Flaps FULL KIAS", a:160, r:""},
-  {s:"Airspeeds", q:"Maximum Tire Speed KGS", a:182, r:""},
-  {s:"Airspeeds", q:"Min Mach for SAT -54°C to-65°C", a:0.55, r:""},
-  {s:"Airspeeds", q:"Min Mach for SAT -65°C to-70°C", a:0.67, r:""},
-  {s:"Electrical", q:"Min Bat Voltage for Start", a:24, r:""},
-  {s:"Electrical", q:"Min GPU Voltage for Bat Charge", a:27, r:""},
-  {s:"Electrical", q:"Generator Load on Ground Amps each", a:330, r:""},
-  {s:"Electrical", q:"Generator Load in Flight Amps each", a:390, r:""},
-  {s:"Fuel", q:"Maximum Fuel Capacity lb", a:5401, r:""},
-  {s:"Fuel", q:"Maximum Usable Fuel Capacity lb", a:5352, r:""},
-  {s:"Fuel", q:"Max Usable Fuel per Tank lb", a:2676, r:""},
-  {s:"Fuel", q:"Unusable Fuel per Tank lb", a:24.2, r:""},
-  {s:"Fuel", q:"Maximum Fuel Imbalance lb", a:220, r:""},
-  {s:"Fuel", q:"Minimum Fuel Temp °C", a:-37, r:""},
-  {s:"Fuel", q:"Maximum Fuel Temp °C (Ground)", a:52, r:""},
-  {s:"Fuel", q:"Maximum Fuel Temp °C (In Flight)", a:80, r:""},
-  {s:"Fuel", q:"Inop Jet Pump Unusable Increase lb", a:44, r:""},
-  {s:"Pressurization", q:"Maximum Differential Pressure psid", a:9.4, r:""},
-  {s:"Pressurization", q:"Maximum DifferentialOverpressure psid", a:9.6, r:""},
-  {s:"Pressurization", q:"Maximum Differential T/L psid", a:0.2, r:""},
-  {s:"Pressurization", q:"Maximum Negative Differential psid", a:-0.3, r:""},
-  {s:"Icing Conditions", q:"Max T/O & Landing PA ft", a:10000, r:""},
-  {s:"Icing Conditions", q:"Max Cruise Alt OEI ft", a:15000, r:""},
-  {s:"Icing Conditions", q:"Minimum Temp for Landing°C", a:-38, r:""},
-  {s:"Icing Conditions", q:"Min Airspeed WHSAIS Inhibited KIAS", a:165, r:""},
-  {s:"Icing Conditions", q:"Min Airspeed WHSAIS Un-Inhibited KIAS", a:150, r:""},
-  {s:"Starter Limits", q:"Cool down after 1st start min(s)", a:1, r:""},
-  {s:"Starter Limits", q:"Cool down after 2nd start min(s)", a:1, r:""},
-  {s:"Starter Limits", q:"Cool down after 3rd start min(s)", a:15, r:""},
-  {s:"Starter Limits", q:"Cool down after 4th start min(s)", a:30, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Starting ITT °C (5 Sec)", a:765, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum ITT °C (10 Min SE in ICE)", a:725, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Takeoff ITT °C (5 Min)", a:700, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous ITT °C", a:680, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Transient ITT °C (20 Sec)", a:765, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum N1% (10 Min SE in ICE)", a:100, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Takeoff N1% (5 Min)", a:100, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous N1%", a:100, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Transient N1% (20 Sec)", a:102, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum N2% (5 Min SE in ICE)", a:101, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Takeoff N2% (5 Min)", a:101, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous N2%", a:101, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Minimum Ground Idle N2%", a:55.1, r:""},
-  {s:"Powerplant (FADEC 4.3)", q:"Transient N2% (20 Sec)", a:103, r:""},
-  {s:"Loading", q:"Forward Baggage Compartment lb", a:110, r:""},
-  {s:"Loading", q:"Aft Baggage Compartment lb", a:463, r:""},
-  {s:"Loading", q:"Forward Baggage Load Limit lb/sq ft", a:22.1, r:""},
-  {s:"Loading", q:"Aft Baggage Load Limit lb/sq ft", a:70.0, r:""},
-  {s:"Auto Pilot", q:"Minimum Engage Height ft agl (2 Eng)", a:600, r:""},
-  {s:"Auto Pilot", q:"Minimum Engage Height ft agl (1 Eng)", a:1000, r:""},
-  {s:"Auto Pilot", q:"Minimum Use Height ft agl (2 Eng)", a:195, r:""},
-  {s:"Auto Pilot", q:"Minimum Use Height ft agl (1 Eng)", a:220, r:""},
-  {s:"Miscellaneous", q:"Maximum Cruise Altitude ft", a:45000, r:""},
-  {s:"Miscellaneous", q:"Max Alt ft for Flap Extended", a:18000, r:""},
-  {s:"Miscellaneous", q:"Max Altitude ft PA for T/O & Landing", a:14000, r:""},
-  {s:"Miscellaneous", q:"Load Factors g Flaps 0", a:3.00, r:""},
-  {s:"Miscellaneous", q:"Load Factors g Flaps 1, 2, 3 & FULL", a:2.00, r:""},
-  {s:"Miscellaneous", q:"Maximum Runway Slope %", a:2.0, r:""},
-  {s:"Miscellaneous", q:"Max Tailwind Component kts", a:10, r:""},
-  {s:"Miscellaneous", q:"Max Crosswind kts for Static T/O", a:18, r:""},
-  {s:"Miscellaneous", q:"Min Temp °C for T/O & Landing", a:-40, r:""},
+  // {s:"Airspeeds", q:"VLO KIAS", a:250, r:""},
+  // {s:"Airspeeds", q:"VLE KIAS", a:250, r:""},
+  // {s:"Airspeeds", q:"Takeoff VMC KIAS", a:97, r:""},
+  // {s:"Airspeeds", q:"Landing VMC (No Icing Conditions) KIAS", a:91, r:""},
+  // {s:"Airspeeds", q:"Landing VMC (Icing Conditions) KIAS", a:95, r:""},
+  // {s:"Airspeeds", q:"MMO", a:0.78, r:""},
+  // {s:"Airspeeds", q:"VO KIAS", a:205, r:""},
+  // {s:"Airspeeds", q:"VFE Flaps 1 KIAS", a:180, r:""},
+  // {s:"Airspeeds", q:"VFE Flaps 2&3 KIAS", a:170, r:""},
+  // {s:"Airspeeds", q:"VFE Flaps FULL KIAS", a:160, r:""},
+  // {s:"Airspeeds", q:"Maximum Tire Speed KGS", a:182, r:""},
+  // {s:"Airspeeds", q:"Min Mach for SAT -54°C to-65°C", a:0.55, r:""},
+  // {s:"Airspeeds", q:"Min Mach for SAT -65°C to-70°C", a:0.67, r:""},
+  // {s:"Electrical", q:"Min Bat Voltage for Start", a:24, r:""},
+  // {s:"Electrical", q:"Min GPU Voltage for Bat Charge", a:27, r:""},
+  // {s:"Electrical", q:"Generator Load on Ground Amps each", a:330, r:""},
+  // {s:"Electrical", q:"Generator Load in Flight Amps each", a:390, r:""},
+  // {s:"Fuel", q:"Maximum Fuel Capacity lb", a:5401, r:""},
+  // {s:"Fuel", q:"Maximum Usable Fuel Capacity lb", a:5352, r:""},
+  // {s:"Fuel", q:"Max Usable Fuel per Tank lb", a:2676, r:""},
+  // {s:"Fuel", q:"Unusable Fuel per Tank lb", a:24.2, r:""},
+  // {s:"Fuel", q:"Maximum Fuel Imbalance lb", a:220, r:""},
+  // {s:"Fuel", q:"Minimum Fuel Temp °C", a:-37, r:""},
+  // {s:"Fuel", q:"Maximum Fuel Temp °C (Ground)", a:52, r:""},
+  // {s:"Fuel", q:"Maximum Fuel Temp °C (In Flight)", a:80, r:""},
+  // {s:"Fuel", q:"Inop Jet Pump Unusable Increase lb", a:44, r:""},
+  // {s:"Pressurization", q:"Maximum Differential Pressure psid", a:9.4, r:""},
+  // {s:"Pressurization", q:"Maximum DifferentialOverpressure psid", a:9.6, r:""},
+  // {s:"Pressurization", q:"Maximum Differential T/L psid", a:0.2, r:""},
+  // {s:"Pressurization", q:"Maximum Negative Differential psid", a:-0.3, r:""},
+  // {s:"Icing Conditions", q:"Max T/O & Landing PA ft", a:10000, r:""},
+  // {s:"Icing Conditions", q:"Max Cruise Alt OEI ft", a:15000, r:""},
+  // {s:"Icing Conditions", q:"Minimum Temp for Landing°C", a:-38, r:""},
+  // {s:"Icing Conditions", q:"Min Airspeed WHSAIS Inhibited KIAS", a:165, r:""},
+  // {s:"Icing Conditions", q:"Min Airspeed WHSAIS Un-Inhibited KIAS", a:150, r:""},
+  // {s:"Starter Limits", q:"Cool down after 1st start min(s)", a:1, r:""},
+  // {s:"Starter Limits", q:"Cool down after 2nd start min(s)", a:1, r:""},
+  // {s:"Starter Limits", q:"Cool down after 3rd start min(s)", a:15, r:""},
+  // {s:"Starter Limits", q:"Cool down after 4th start min(s)", a:30, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Starting ITT °C (5 Sec)", a:765, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum ITT °C (10 Min SE in ICE)", a:725, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Takeoff ITT °C (5 Min)", a:700, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous ITT °C", a:680, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Transient ITT °C (20 Sec)", a:765, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum N1% (10 Min SE in ICE)", a:100, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Takeoff N1% (5 Min)", a:100, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous N1%", a:100, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Transient N1% (20 Sec)", a:102, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum N2% (5 Min SE in ICE)", a:101, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Takeoff N2% (5 Min)", a:101, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Maximum Continuous N2%", a:101, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Minimum Ground Idle N2%", a:55.1, r:""},
+  // {s:"Powerplant (FADEC 4.3)", q:"Transient N2% (20 Sec)", a:103, r:""},
+  // {s:"Loading", q:"Forward Baggage Compartment lb", a:110, r:""},
+  // {s:"Loading", q:"Aft Baggage Compartment lb", a:463, r:""},
+  // {s:"Loading", q:"Forward Baggage Load Limit lb/sq ft", a:22.1, r:""},
+  // {s:"Loading", q:"Aft Baggage Load Limit lb/sq ft", a:70.0, r:""},
+  // {s:"Auto Pilot", q:"Minimum Engage Height ft agl (2 Eng)", a:600, r:""},
+  // {s:"Auto Pilot", q:"Minimum Engage Height ft agl (1 Eng)", a:1000, r:""},
+  // {s:"Auto Pilot", q:"Minimum Use Height ft agl (2 Eng)", a:195, r:""},
+  // {s:"Auto Pilot", q:"Minimum Use Height ft agl (1 Eng)", a:220, r:""},
+  // {s:"Miscellaneous", q:"Maximum Cruise Altitude ft", a:45000, r:""},
+  // {s:"Miscellaneous", q:"Max Alt ft for Flap Extended", a:18000, r:""},
+  // {s:"Miscellaneous", q:"Max Altitude ft PA for T/O & Landing", a:14000, r:""},
+  // {s:"Miscellaneous", q:"Load Factors g Flaps 0", a:3.00, r:""},
+  // {s:"Miscellaneous", q:"Load Factors g Flaps 1, 2, 3 & FULL", a:2.00, r:""},
+  // {s:"Miscellaneous", q:"Maximum Runway Slope %", a:2.0, r:""},
+  // {s:"Miscellaneous", q:"Max Tailwind Component kts", a:10, r:""},
+  // {s:"Miscellaneous", q:"Max Crosswind kts for Static T/O", a:18, r:""},
+  // {s:"Miscellaneous", q:"Min Temp °C for T/O & Landing", a:-40, r:""},
   {s:"Miscellaneous", q:"Maximum ISA Deviation ISA+°C", a:35, r:""}
   ] ;
 
@@ -92,6 +85,7 @@ var i = 0; // set to first question/answer.
 var input = "";  // answer_box input will go here
 var last_question = data.length; // set last question reference
 var timer_started_flag = false;
+var strikes = 0; // count wrong answers
 const question_section = document.getElementById('question_section');
 const question_box = document.getElementById('question_box');
 const answer_box = document.getElementById('answer_box');
@@ -110,7 +104,7 @@ function start_timer(){
       elapsed = Math.floor(time / 100) / 10;
       if(Math.round(elapsed) == elapsed) { elapsed += '.0'; }
 
-      document.title = elapsed;
+      document.getElementById('timer').innerHTML = elapsed;
 
   }, 100);
 }
@@ -137,6 +131,9 @@ answer_box.addEventListener("keyup", function(event){
     }
     else {
       document.getElementById('work_area').style.backgroundColor = 'rgba(255, 165, 0, 0.8)';
+      if (strikes > 1){
+        info_box.innerHTML = "The answer is: " + data[i].a;
+      }
       answer_box.select();
     }
   }
@@ -154,14 +151,15 @@ function ask_question(n){
 function check_answer(my_answer){
   var correct_answer = data[i].a;
     if (my_answer == correct_answer){
+      strikes = 0;
       result = "correct";
     }
     else{
+      strikes++;
       result = "incorrect";
     }
     // if timer not already, start timer
     if ( timer_started_flag == false ){
-      console.log("start timer now");
       start_timer();
       timer_started_flag = true;
     }
@@ -169,7 +167,8 @@ function check_answer(my_answer){
   }
 
 function end_quiz(){
-  info_box.innerHTML = "You have finished the quiz in " + elapsed + " seconds.";
+  let end_time = document.getElementById('timer').innerHTML;
+  info_box.innerHTML = "You have finished the quiz in " + end_time + " seconds.";
 }
 
 function shuffle(array) {
